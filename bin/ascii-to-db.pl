20f7c7e85b43e59a21cceffd815b5fa4446a3712
David Chiang <dchiang@nd.edu>
2019-03-05 15:24:48 -0500

    Follows up on previous modifications made at Softconf to allow more Unicode in the db file.
    Unicode:
    - eliminated two remaining cases of Unicode-to-TeX conversion (inputenc handles both of them)
    - NFKC normalization
    - generalized handling of fi ligature to all ligatures. Also all halfwidth/fullwidth forms. There may be other character classes too.
    
    TeX:
    - fixed an edge case in handling of `_ & ^` (it didn't work at the beginning of line)
    - restrict escaping of `^ _` to outside of math mode
    - also escape %
    - removed `{}` after `\^`
    - removed escaping of `@`. This should be done when generating BibTeX only.
    - simpler handling of `#` to avoid `#==#` and `#=%=#` (used as separators in START metadata)
    
    Escaping `{ } $ ~` is hard and left for future work...

--- bin/ascii-to-db.pl
+++ bin/ascii-to-db.pl
@@ -1,31 +1,50 @@
 #!/usr/bin/perl
 
-# Filters a metadata file from START, to translate its
-# accented characters into latex format for the DB file.
+# Filters a paper metadata file from START.
 
 use utf8;
 use open qw(:std :utf8);
 
+use Unicode::Normalize;
+
+sub convert {
+    my $s = $_[0];
+
+    ### Normalize Unicode
+
+    # We want NFC with *some* aspects of NFKC normalization.
+    
+    # We want:
+    # - fullwidth characters sometimes used with CJK
+    # - ligatures like ﬁ that come from copy-and-pasting PDF files
+    # But we don't want (for example):
+    # - subscripts and superscripts (²)
+    
+    $s =~ s/(\p{Halfwidth_and_Fullwidth_Forms}|\p{Alphabetic Presentation Forms})/NFKD($1)/ge;
+    $s = NFC($s);
+
+    ### Handle characters that have a special meaning in TeX.
+    
+    # & # % are unlikely to be used with their TeX meanings, so escape them.
+    $s =~ s/(?<!\\)([&#%])/\\$1/g;
+
+    # ^ and _ should only be escaped outside of math mode, so hide math, escape, unhide math.
+    $s =~ s/\$(.*?)\$/'$'.unpack("H*",$1).'$'/ge;
+    $s =~ s/(?<!\\)([\^_])/\\$1/g;
+    $s =~ s/\$(.*?)\$/'$'.pack("H*",$1).'$'/ge;
+
+    # But { } $ ~ can be used either as ordinary characters or with their TeX meanings.
+    # To do: Rules for determining when to escape these.
+    
+    return $s;
+}
+
 while (<>) {
-
-    s/®/\\textregistered\~/g;
-    s/–/--/g;
-
-    s/ﬁ/fi/g;
-
-    # Latex Chars
-    s/([^\\])(_)/$1\\$2/g;   # unescaped underscore
-    s/([^\\])(\&)/$1\\$2/g;  # unescaped ampersand
-    s/([^\\])(\^)/$1\\^{}/g; # unescaped carrot
-
-    s/([^=#])(\#)([^=#])/${1}\\#${3}/g;    # pound sign - watch out for separator #=%=#
-    s/^(\#)([^=#])/\\#${2}/g;
-    s/([^=#])(\#)$/${1}\\#/g;
-    s/#=%=##/#=%=#\\#/g;
-    s/##=%=#/\\##=%=#/g;
-
-    s/([^\\])(\@)/$1\$@\$/g;  # unescaped at-sign
-
-    print;
-
+    # The START metadata file uses #==# and #=%=# as key/value separators.
+    # We only want to convert the values.
+    if (/^(.*#=%?=#)(.*)$/s) {
+        print $1, convert($2);
+    } else {
+        print convert($_);
+    }
 }
