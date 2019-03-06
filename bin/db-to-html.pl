9708d2db9ac44f4613fcd5d032c17919a02b520d
David Chiang <dchiang@nd.edu>
2019-03-06 12:46:22 -0500

    Although I'm hoping that HTML generation can be dropped, this generalizes db-to-html.pl to a lot more cases. Hopefully without introducing any bugs.

--- bin/db-to-html.pl
+++ bin/db-to-html.pl
@@ -3,16 +3,13 @@
 # Filters a DB (or meta) file so that its entries are more HTML-friendly.
 # The result is still a DB (or meta) file.
 #
-# This file gets most common cases but is still slightly incomplete.
-# Also, the HTML specification does get extended from time to time.
-#
-# Doubtful that the diacritics need to be translated anymore, since HTML
-# is unicode friendly.  However, the latex versions of the diacritics will
-# need to be translated, if any were present in the metadata.
+# There is a module, TeX::Encode, that does most of this, but it
+# currently (Mar 2019) has a bug, mapping (for example) \it to ıt.
 
 use utf8;
 use open qw(:std :utf8);
 
+use Unicode::Normalize;
 use HTML::Entities;
 
 while (<>) {
@@ -22,272 +19,109 @@ while (<>) {
         next;
     }
 
-    s/\r//g;         # kill CR from DOS format files
+    chomp;
+
+    # The following two changes make later processing simpler.
+    
+    # Exactly one space after every control word
+    s/(\\[A-Za-z]+)\s*/$1 /g;
+
+    # Remove curly braces around single characters
+    s/\{([^\\{}])\}/$1/g;
 
     # latex cruft
-    s/\\@//g;          # kill latex \@, sometimes used after periods
-    s/\\,//g;          # kill latex \, sometimes used to widen titles in TOC
-    s/\\\\|\\newline\b/<br>/g;    # latex newlines: convert to <br>
-    s/\\ / /g;         # latex hard space: convert to ordinary space
-    s/(?<!\\)~/ /g;    # latex hard space ~ unless preceded by backslash: convert to ordinary space
-    s/\\&/&/g;         # latex \&
-    s/(?<!\\)~/ /g;    # latex hard space ~ unless preceded by backslash: convert to ordinary space
-
-    # common latex glyphs
-    s/---/&#8212;/g;   # the name "&mdash;" will only be supported in HTML 4
-    s/--/&#8211;/g;    # the name "&ndash;" will only be supported in HTML 4
-    s/(?<!\\)\`\`/&ldquo;/g;  # smart quotes (also single apostrophe), unless preceded by backslash
-    s/(?<!\\)\'\'/&rdquo;/g;
-    s/(?<!\\)\`/&lsquo;/g;
-    s/(?<!\\)\'/&rsquo;/g;
-
-    # collapse whitespace
-    s/[ \t]+/ /g;
-    s/^ //;
-    s/ $//;
-
-    # diacritics.  Pretty good, but:
-    # - Still not quite complete:
-    #   Check out http://www.htmlhelp.com/reference/html40/entities/latin1.html
-    #   and http://molspect.mps.ohio-state.edu/symposium/latexinstruct.html .
-    # - Redundancy is not exploited.
-    #   It may be helpful to use more regexp features, like $1 and /e.
-    # - The optional braces around \'a are not required to match.
-    # - The braces around \AA, etc., should be optional: an alternative
-    #   is to follow with a non-letter (typically whitespace, which should then be deleted).
-    #   In general, the regexps should be rewritten to parse first (the way latex does)
-    #   and interpret second.
-    # - There are some characters, like \c{s}, that don't appear 
-    #   to admit a Latin-1 encoding (see the entry for Dan Tufi\c{s} at W03-0308 
-    #   in http://acl.ldc.upenn.edu/W/W03/ ).
-
-    s/\\\'\{a}/&aacute;/g;
-    s/\\\'\{e}/&eacute;/g;
-    s/\\\'\{i}/&iacute;/g;
-    s/\\\'\{\\i}/&iacute;/g;
-    s/\\\'\{o}/&oacute;/g;
-    s/\\\'\{u}/&uacute;/g;
-    s/\\\'\{y}/&yacute;/g;
-
-    s/\\\'\{n}/&#324;/g;
-
-    s/\\\`\{a}/&agrave;/g;
-    s/\\\`\{e}/&egrave;/g;
-    s/\\\`\{i}/&igrave;/g;
-    s/\\\`\{\\i}/&igrave;/g;
-    s/\\\`\{o}/&ograve;/g;
-    s/\\\`\{u}/&ugrave;/g;
-
-    s/\\\^\{a}/&acirc;/g;
-    s/\\\^\{e}/&ecirc;/g;
-    s/\\\^\{i}/&icirc;/g;
-    s/\\\^\{\\i}/&icirc;/g;
-    s/\\\^\{o}/&ocirc;/g;
-    s/\\\^\{u}/&ucirc;/g;
-
-    s/\\\"\{a}/&auml;/g;
-    s/\\\"\{e}/&euml;/g;
-    s/\\\"\{i}/&iuml;/g;
-    s/\\\"\{\\i}/&iuml;/g;
-    s/\\\"\{o}/&ouml;/g;
-    s/\\\"\{u}/&uuml;/g;
-
-    s/\{\\\"a}/&auml;/g;
-    s/\{\\\"e}/&euml;/g;
-    s/\{\\\"i}/&iuml;/g;
-    s/\{\\\"o}/&ouml;/g;
-    s/\{\\\"u}/&uuml;/g;
-
-    s/\\\~\{a}/&atilde;/g;
-    s/\\\~\{o}/&otilde;/g;
-    s/\\\~\{n}/&ntilde;/g;
-    s/\\\~\{i}/&itilde;/g;
-    s/\\\~\{u}/&utilde;/g;
-    s/\\c\{c}/&ccedil;/g;
-    s/\\c\{s}/&#351;/g;
-
-    s/\\v\{C}/&#268;/g;
-    s/\\v\{c}/&#269;/g;
-    s/\\v\{E}/&#282;/g;
-    s/\\v\{e}/&#283;/g;
-    s/\\v\{N}/&#327;/g;
-    s/\\v\{n}/&#328;/g;
-    s/\\v\{R}/&#344;/g;
-    s/\\v\{r}/&#345;/g;
-    s/\\v\{S}/&#352;/g;
-    s/\\v\{s}/&#353;/g;
-    s/\\v\{Z}/&#381;/g;
-    s/\\v\{z}/&#158;/g;
-    s/\{\\AA}/&Aring;/g;
-    s/\{\\aa}/&aring;/g;
-    s/\{\\AE}/&AElig;/g;
-    s/\{\\ae}/&aelig;/g;
-    s/\{\\ss}/&szlig;/g;
-
-    s/\{?\\\'a}?/&aacute;/g;    
-    s/\{?\\\'e}?/&eacute;/g;
-    s/\{?\\\'i}?/&iacute;/g;
-    s/\{?\\\'\\i}?/&iacute;/g;
-    s/\{?\\\'o}?/&oacute;/g;
-    s/\{?\\\'u}?/&uacute;/g;
-    s/\{?\\\'y}?/&yacute;/g;
-
-    s/\{?\\\`a}?/&agrave;/g;
-    s/\{?\\\`e}?/&egrave;/g;
-    s/\{?\\\`i}?/&igrave;/g;
-    s/\{?\\\`\\i}?/&igrave;/g;
-    s/\{?\\\`o}?/&ograve;/g;
-    s/\{?\\\`u}?/&ugrave;/g;
-
-    s/\{?\\\^a}?/&acirc;/g;
-    s/\{?\\\^e}?/&ecirc;/g;
-    s/\{?\\\^i}?/&icirc;/g;
-    s/\{?\\\^\\i}?/&icirc;/g;
-    s/\{?\\\^o}?/&ocirc;/g;
-    s/\{?\\\^u}?/&ucirc;/g;
-
-    s/\{?\\\"a}?/&auml;/g;
-    s/\{?\\\"e}?/&euml;/g;
-    s/\{?\\\"i}?/&iuml;/g;
-    s/\{?\\\"\\i}?/&iuml;/g;
-    s/\{?\\\"o}?/&ouml;/g;
-    s/\{?\\\"u}?/&uuml;/g;
-
-    s/\{?\\\~a}?/&atilde;/g;
-    s/\{?\\\~o}?/&otilde;/g;
-    s/\{?\\\~n}?/&ntilde;/g;
-
-    s/\\\'\{A}/&Aacute;/g;
-    s/\\\'\{E}/&Eacute;/g;
-    s/\\\'\{I}/&Iacute;/g;
-    s/\\\'\{O}/&Oacute;/g;
-    s/\\\'\{U}/&Uacute;/g;
-    s/\\\'\{Y}/&Yacute;/g;
-
-    s/\\\`\{A}/&Agrave;/g;
-    s/\\\`\{E}/&Egrave;/g;
-    s/\\\`\{I}/&Igrave;/g;
-    s/\\\`\{O}/&Ograve;/g;
-    s/\\\`\{U}/&Ugrave;/g;
-
-    s/\\\^\{A}/&Acirc;/g;
-    s/\\\^\{E}/&Ecirc;/g;
-    s/\\\^\{I}/&Icirc;/g;
-    s/\\\^\{O}/&Ocirc;/g;
-    s/\\\^\{U}/&Ucirc;/g;
-
-    s/\\\"\{A}/&Auml;/g;
-    s/\\\"\{E}/&Euml;/g;
-    s/\\\"\{I}/&Iuml;/g;
-    s/\\\"\{O}/&Ouml;/g;
-    s/\\\"\{U}/&Uuml;/g;
-
-    s/\{\\o\}/&oslash;/g; 
-    s/\{\\O\}/&Oslash;/g;  
-
-    s/\\o/&oslash;/g;
-    s/\\O/&Oslash;/g;
-    s/\\u\{g}/&#287;/g;
-
-    s/\\\~\{A}/&Atilde;/g;
-    s/\\\~\{O}/&Otilde;/g;
-    s/\\\~\{N}/&Ntilde;/g;
-    s/\\c\{C}/&Ccedil;/g;
-    s/\\\'\{S}/&#346;/g;
-    s/\\\'\{C}/&#262;/g;
-    s/\\\'\{s}/&#347;/g;
-    s/\\\'\{c}/&#263;/g;
-
-    s/\{?\\\'C}?/&#262;/g;
-    s/\{?\\\'c}?/&#263;/g;
-    s/\{?\\\'S}?/&#346;/g;
-    s/\{?\\\'s}?/&#347;/g;
-
-    s/\\\=\{a}/&#257;/g;
-    s/\\\=\{e}/&#275;/g;
-    s/\\\=\{i}/&#299;/g;
-    s/\\\=\{o}/&#333;/g;
-    s/\\\=\{u}/&#363;/g;
-
-    s/\\\=\{A}/&#256;/g;
-    s/\\\=\{E}/&#274;/g;
-    s/\\\=\{I}/&#298;/g;
-    s/\\\=\{O}/&#332;/g;
-    s/\\\=\{U}/&#362;/g;
-
-    s/\{\\L\}/&#0321/g; 
-    s/\{\\l\}/&#0322/g; 
-
-    s/\{\\l}/&#322;/g;
-
-    s/\\v\{a}/&#259;/g;
-
-    s/\\\=\{e}/&#275;/g;
-    s/\\\=\{u}/&#363;/g;
-    s/\\\=\{\\\i}/&#299;/g;
-    s/\\\=\{a}/&#257;/g;
-
-    s/\\v\{s}/&#353;/g;
-    s/\\v\{g}/&#289;/g;
-
-    s/\\c\{k}/&#311;/g;
-    s/\\c\{l}/&#316;/g;
-
-    s/\\v\{z}/&#158;/g;
-    s/\\v\{c}/&#269;/g;
-
-    s/\\c\{n}/&#316;/g;
-
-    s/\\\=\{E}/&#274;/g;
-    s/\\\=\{U}/&#362;/g;
-    s/\\\=\{I}/&#298;/g;
-    s/\\\=\{A}/&#256;/g;
-
-    s/\\v\{S}/&#352;/g;
-
-    s/\\c\{G}/&#290;/g;
-    s/\\c\{K}/&#310;/g;
-    s/\\c\{L}/&#315;/g;
-
-    s/\\v\{Z}/&#381;/g;
-    s/\\v\{C}/&#268;/g;
-
-    s/\\c\{N}/&#325;/g;
-    s/\\c\{S}/&#350;/g;
-
+    s/\\@//g;          # \@, sometimes used after periods
+    s/\\-//g;          # soft hyphen
+    s/\\,//g;          # \, sometimes used to widen titles in TOC
+    s/\\ / /g;         # hard space: convert to ordinary space
+    s/(?<!\\)~/ /g;    # tie (~) unless preceded by backslash: convert to ordinary space
+    
     # Latex chars
-    s/\\_/_/g;     # Underscore.
-                   # Ampersand done above.
-    s/\Q\^{}\E/&#94;/g; # Caret
-    s/\Q\#\E/&#35;/g;   # Pound
-    s/\$@\$/&#64;/g;    # AT
+    s/\\&/&/g;
+    s/\\#/#/g;
+    s/\\%/%/g;
+    s/\\\^\{}/^/g;
+    s/\\_/_/g;
+    s/\\\$/\$/g;
+    
+    # common latex glyphs
+    s/---/—/g;
+    s/--/–/g;
+    s/(?<!\\)``/“/g;  # smart quotes (also single apostrophe), unless preceded by backslash
+    s/(?<!\\)''/”/g;
+    s/(?<!\\)`/‘/g;
+    s/(?<!\\)'/’/g; # bug: 'foo' -> ’foo’
 
+    s/\\AA /Å/g;
+    s/\\aa /å/g;
+    s/\\AE /Æ/g;
+    s/\\ae /æ/g;
+    s/\\DH /Ð/g;
+    s/\\dh /ð/g;
+    s/\\i /ı/g; # keep this before translation of accents
+    s/\\L /Ł/g; 
+    s/\\l /ł/g;
+    s/\\O /Ø/g;  
+    s/\\o /ø/g; 
+    s/\\ss /ß/g;
+    s/\\TH /Þ/g;
+    s/\\th /þ/g;
+
+    # Convert TeX accents to Unicode combining accents (which come after)
+    s/\\'\s*([^\\{}])/$1\N{COMBINING ACUTE ACCENT}/g;
+    s/\\`\s*([^\\{}])/$1\N{COMBINING GRAVE ACCENT}/g;
+    s/\\\^\s*([^\\{}])/$1\N{COMBINING CIRCUMFLEX ACCENT}/g;
+    s/\\~\s*([^\\{}])/$1\N{COMBINING TILDE}/g;
+    s/\\=\s*([^\\{}])/$1\N{COMBINING MACRON}/g;
+    s/\\u ([^\\{}])/$1\N{COMBINING BREVE}/g;
+    s/\\d ([^\\{}])/$1\N{COMBINING DOT ABOVE}/g;
+    s/\\"\s*([^\\{}])/$1\N{COMBINING DIAERESIS}/g;
+    s/\\r ([^\\{}])/$1\N{COMBINING RING ABOVE}/g;
+    s/\\H ([^\\{}])/$1\N{COMBINING DOUBLE ACUTE ACCENT}/g;
+    s/\\v ([^\\{}])/$1\N{COMBINING CARON}/g;
+    s/\\\.\s*([^\\{}])/$1\N{COMBINING DOT BELOW}/g;
+    s/\\c ([^\\{}])/$1\N{COMBINING CEDILLA}/g;
+    s/\\k ([^\\{}])/$1\N{COMBINING OGONEK}/g;
+
+    # Convert letter + combining accent to composed character
+    $_ = NFC($_);
+    
+    # HTML escaping
+    $_ = encode_entities($_, '<>&"\'');
+
+    # Now introduce HTML tags
+
+    s/\\\\|\\newline /<br>/g;    # latex newlines: convert to <br>
+    
     do {
         $in = $_;		# process innermost tags until none left
-        # italicization
         # !!! could also try to fix math, e.g., "$n$-gram"
-        s/{\\em ([^\{\}]+)}/$1/g;
-        s/\\textit\{([^\{\}]+)}/$1/g;
-        s/\\emph\{([^\{\}]+)}/$1/g;
+        
+        # italics
+        s/\\em ([^{}]+)(?=})/<i>$1<\/i>/g;
+        s/\\emph \{([^{}]+)}/<i>$1<\/i>/g;
+        s/\\it ([^{}]+)(?=})/<i>$1<\/i>/g;
+        s/\\textit \{([^{}]+)}/<i>$1<\/i>/g;
 
         # boldface
-        s/{\\bf ([^\{\}]+)}/$1/g;
-        s/\\textbf\{([^\{\}]+)}/$1/g;
+        s/\\bf ([^{}]+)(?=})/<b>$1<\/b>/g;
+        s/\\textbf \{([^{}]+)}/<b>$1<\/b>/g;
 
         # small caps - just print normally
-        s/\\textsc\{([^\{\}]+)}/$1/g;
+        s/\\sc ([^{}]+)(?=})/$1/g;
+        s/\\textsc \{([^{}]+)}/$1/g;
     } until ($in eq $_);
-
-    # Any remaining backslashed sequences get deleted with a WARNING
-    warn "Don't know how to translate $& to HTML; deleting it" while s/\\[A-Za-z]+//;
 
     # eliminate any remaining curly braces (usually used to protect capitalization in bibtex).
     # Unless preceded by backslash.
     s/(?<!\\)[{}]//g;
+    
+    s/\\\{/{/g;
+    s/\\\}/}/g;
 
-    # Finally, convert HTML entities to their Unicode equivalents.
-    $_ = decode_entities($_);
+    # Any remaining backslashed sequences get deleted with a WARNING
+    warn "Don't know how to translate $& to HTML; deleting it" while s/\\[A-Za-z]+ |\\.//;
 
-    print;
+    print "$_\n";
 
 }
