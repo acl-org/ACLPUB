45dc2e9a37e00d29211c93e429ae9e94564f9cdf
Matt Post <post@cs.jhu.edu>
2019-05-21 09:55:38 -0400

    remove windows linefeeds

--- anthologize/anthologize.pl
+++ anthologize/anthologize.pl
@@ -29,6 +29,8 @@ my ($abbrev, $year, $bib_url);
 open(META, "$proceedings/meta") || die "couldn't read meta file";
 while (<META>) {
     chomp;
+    # clean up after windows users
+    $_ =~ s/\R//g;
     $abbrev = $1 if (/^abbrev\s+(.*)/);
     $year = $1 if (/^year\s+(.*)/);
     $bib_url = $1 if (/^bib_url\s+(.*)/);
@@ -56,7 +58,7 @@ make_path($anth_dir);
 #       N18-1.pdf for the entire volume
 
 my $bib = "$dir/$abbrev-$year.bib";
--f $bib || die "couldn't find $bib";
+-f $bib || die "couldn't find $bib\n";
 my $bibdst = "$anth_dir/$volume_idno.bib";
 symlink(File::Spec->abs2rel($bib, $anth_dir), $bibdst) || die "couldn't link $bibdst -> $bib";
 print STDERR "$bibdst -> $bib\n" if $verbose;
